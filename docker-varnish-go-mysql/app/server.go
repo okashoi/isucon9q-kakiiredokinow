71d63a03c6b436b2592a3986cff32de4f4dfdf96
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2019-08-09 21:34:16 +0900

    feat: golang appからmysqlへつないだ

---
+++ docker-varnish-go-mysql/app/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(db:3306)/app-db")
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if err := db.Ping(); err != nil {
+			log.Println("err", err)
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "db error.")
+			return
+		}
+
+		fmt.Fprintf(w, "Hello, world!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
